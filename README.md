02d14e779a5ce4a8cd9eb63ae2010e24b11dce16
sehnis <sehnis@umich.edu>
2018-03-11 08:17:15 -0400

    API Working

---
+++ README.md
@@ -0,0 +1,44 @@
+### 364Games -- Midterm Project for SI 364
+
+This is a web application that uses the GiantBomb video game REST API to allow users to search for and review games. 
+
+### Requirements to complete for 1800 points (90%) -- an awesome, solid app
+
+*(I recommend treating this as a checklist and checking things off as you get them done!)*
+
+#### Documentation Requirements (so we can grade the assignments)
+
+* **Note:** See **To Submit** for submission instructions.
+* Create a `README.md` file for your app that includes the full list of requirements from this page. The ones you have completed should be bolded. (You bold things in Markdown by using two asterisks, like this: `**This text would be bold** and this text would not be`)
+* The `README.md` file should include a list of all of the routes that exist in the app and the names of the templates each one should render (e.g. `/form` -> `form.html`, like [the list we provided in the instructions for HW2](https://www.dropbox.com/s/3a83ykoz79tqn8r/Screenshot%202018-02-15%2013.27.52.png?dl=0)).
+* ** The `README.md` file should contain at least 1 line of description of what your app is about or should do.**
+
+#### Code Requirements
+
+**Note that many of these requirements go together!**
+
+**- [ ] Ensure that the `SI364midterm.py` file has all the setup (`app.config` values, import statements, code to run the app if that file is run, etc) necessary to run the Flask application, and the application runs correctly on `http://localhost:5000` (and the other routes you set up)**
+- [ ] Add navigation in `base.html` with links (using `a href` tags) that lead to every other viewable page in the application. (e.g. in the lecture examples from the Feb 9 lecture, [like this](https://www.dropbox.com/s/hjcls4cfdkqwy84/Screenshot%202018-02-15%2013.26.32.png?dl=0) )
+**- [ ] Ensure that all templates in the application inherit (using template inheritance, with `extends`) from `base.html` and include at least one additional `block`.**
+**- [ ] Include at least 2 additional template `.html` files we did not provide.**
+**- [ ] At least one additional template with a Jinja template for loop and at least one additional template with a Jinja template conditional.
+    - These could be in the same template, and could be 1 of the 2 additional template files.**
+**- [ ] At least one errorhandler for a 404 error and a corresponding template.**
+**- [ ] At least one request to a REST API that is based on data submitted in a WTForm.**
+- [ ] At least one additional (not provided) WTForm that sends data with a `GET` request to a new page.
+**- [ ] At least one additional (not provided) WTForm that sends data with a `POST` request to the *same* page.**
+**- [ ] At least one custom validator for a field in a WTForm.**
+- [ ] At least 2 additional model classes.
+- [ ] Have a one:many relationship that works properly built between 2 of your models.
+- [ ] Successfully save data to each table.
+- [ ] Successfully query data from each of your models (so query at least one column, or all data, from every database table you have a model for).
+- [ ] Query data using an `.all()` method in at least one view function and send the results of that query to a template.
+- [ ] Include at least one use of `redirect`. (HINT: This should probably happen in the view function where data is posted...)
+- [ ] Include at least one use of `url_for`. (HINT: This could happen where you render a form...)
+- [ ] Have at least 3 view functions that are not included with the code we have provided. (But you may have more! *Make sure you include ALL view functions in the app in the documentation and ALL pages in the app in the navigation links of `base.html`.*)
+
+### Additional Requirements for an additional 200 points (to reach 100%) -- an app with extra functionality!
+
+* (100 points) Include an *additional* model class (to make at least 4 total in the application) with at least 3 columns. Save data to it AND query data from it; use the data you query in a view-function, and as a result of querying that data, something should show up in a view. (The data itself should show up, OR the result of a request made with the data should show up.)
+
+**- (100 points) Write code in your Python file that will allow a user to submit duplicate data to a form, but will *not* save duplicate data (like the same user should not be able to submit the exact same tweet text for HW3).**
--- SI364midterm.py
+++ SI364midterm.py
@@ -7,9 +7,14 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
+from wtforms import StringField, SelectField, SubmitField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
 from wtforms.validators import Required # Here, too
 from flask_sqlalchemy import SQLAlchemy
+import json
+import requests
+
+# Pybomb is an API Wrapper for the GiantBomb REST API.
+import pybomb
 
 ## App setup code
 app = Flask(__name__)
@@ -17,7 +22,8 @@ app.debug = True
 app.use_reloader = True
 
 ## All app.config values
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///364midterm_sehnis.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 ## Statements for db setup (and manager setup if using Manager)
 db = SQLAlchemy(app)
@@ -27,22 +33,63 @@ db = SQLAlchemy(app)
 ######## HELPER FXNS (If any) ########
 ######################################
 
-
-
-
 ##################
 ##### MODELS #####
 ##################
 
-class Name(db.Model):
-    __tablename__ = "names"
+class Username(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64))
+    userel = db.relationship('Review', backref='users')
 
     def __repr__(self):
         return "{} (ID: {})".format(self.name, self.id)
 
+class Game(db.Model):
+    __tablename__ = "games"
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(128))
+    tagline = db.Column(db.String(64))
+    rating = db.Column(db.String(64))
+    platforms = db.Column(db.String(256))
+    gamerel = db.relationship('Review', backref='games')
 
+    def __init__(self, dictin):
+        self.name = dictin['name']
+        self.tagline = dictin['tagline']
+        self.rating = dictin['rating']
+        self.platforms = dictin['platforms']
+
+    def __repr__(self):
+        return "{} (ID: {})".format(self.name, self.id)
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer,primary_key=True)
+    game = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+    reviewer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rating = db.Column(db.Integer)
+    description = db.Column(db.String(512))
+    tagrel = db.relationship('Tag', backref='reviews')
+
+    def __init__(self, g, re, ra, d):
+        self.game = g
+        self.reviewer = re
+        self.rating = ra
+        self.description = d
+
+    def __repr__(self):
+        return "{} (ID: {})".format(self.name, self.id)
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+    id = db.Column(db.Integer,primary_key=True)
+    review = db.Column(db.Integer, db.ForeignKey('reviews.id'), nullable=False)
+    tagtext = db.Column(db.String(64))
+
+    def __repr__(self):
+        return "{} (ID: {})".format(self.name, self.id)
 
 ###################
 ###### FORMS ######
@@ -52,29 +99,122 @@ class NameForm(FlaskForm):
     name = StringField("Please enter your name.",validators=[Required()])
     submit = SubmitField()
 
+class GameForm(FlaskForm):
+    name = StringField("Name of the Game: ",validators=[])
+    platform = SelectField("Platform: ", choices=[('94', 'PC'), ('146', 'PlayStation 4'), ('17', 'Mac'), ('145', 'XBox One')], validators=[Required()])
+    sorthow = SelectField("How to sort results: ", choices=[('name', 'By Name'), ('number_of_user_reviews', 'By Review Count'), ('date_last_updated', 'By Update Date')], validators=[Required()])
+    submit = SubmitField()
 
+class ReviewForm(FlaskForm):
+    name = StringField("What is the full name of the Game?: ", validators=[Required()])
+    reviewer = StringField("Who are you reviewing this game as?: ", validators=[Required()])
+    score = StringField("Your Score out of 10: ", validators=[Required()])
+    desc = StringField("Please enter the text of your review: ")
+    tags = StringField("Please enter any tags for your review, separated by commas: ")
+    submit = SubmitField()
+
+    def score_validator(form, field):
+        if int(score.data) > 10 | int(score.data) < 1 :
+            raise ValidationError('ERROR -- Your score must be between 1 and 10.')
+
+class SearchForm(FlaskForm):
+    score = StringField("Your Score out of 10: ")
+    desc = StringField("Please enter the text of your review: ")
+    submit = SubmitField()
 
 #######################
 ###### VIEW FXNS ######
 #######################
 
-@app.route('/')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
     if form.validate_on_submit():
         name = form.name.data
-        newname = Name(name)
+        newname = Username(name=name)
         db.session.add(newname)
         db.session.commit()
+        return redirect(url_for('search'))
+    return render_template('base.html',form=form)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = GameForm()
+    if form.validate_on_submit():
+
+        my_key = ''
+        games_client = pybomb.GamesClient(my_key)
+        response = games_client.search(
+            filter_by={'name': form.name.data, 'platforms': form.platform.data},
+            return_fields=('name', 'deck', 'original_game_rating', 'platforms'),
+            sort_by=form.sorthow.data,
+            desc=False,
+            limit=25
+        )
+
+        found_games = []
+        for indiv in response.results:
+            result = {}
+            result['name'] = indiv['name']
+            result['tagline'] = indiv['deck']
+            if indiv['original_game_rating']:
+                result['rating'] = indiv['original_game_rating'][0]['name']
+            else:
+                result['rating'] = 'Unavailable'
+            result['platforms'] = ''
+            for p in indiv['platforms']:
+                result['platforms'] += (p['name'] + " | ")
+            found_games.append(result)
+
+            new_game = Game(result)
+            if not Game.query.filter_by(name=result['name']).first():
+                db.session.add(new_game)
+                db.session.commit()
+
+        return render_template('game_results.html', results=found_games)
+    return render_template('home.html',form=form)
+
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    form = GameForm()
+    if form.validate_on_submit():
+        rev_name = form.reviewer.data
+        game_name = form.name.data
+        rating = form.rating.data
+        if form.desc.data:
+            rev_text = form.desc.data
+        else:
+            rev_text = 'No rationale given.'
+        new_review = Review(game_name, rev_name, rating, rev_text)
+        db.session.add(new_review)
+        db.session.commit()
+
         return redirect(url_for('all_names'))
     return render_template('base.html',form=form)
 
+
+@app.route('/results')
+def show_games():
+    return render_template('games_results.html')
+
 @app.route('/names')
 def all_names():
-    names = Name.query.all()
+    names = Username.query.all()
     return render_template('name_example.html',names=names)
 
+@app.route('/games')
+def all_games():
+    games = Game.query.all()
+    return render_template('games_list.html',games=games)
 
+@app.route('/reviews')
+def last_reviews():
+    revs = Review.query.all()[:10]
+    return render_template('last_reviews.html',reviews=revs)
 
 
 
@@ -83,3 +223,7 @@ def all_names():
 
 # Put the code to do so here!
 # NOTE: Make sure you include the code you need to initialize the database structure when you run the application!
+if __name__ == '__main__':
+    db.create_all()
+    app.secret_key = 'somethingsecretthatillmakebetterlater'
+    app.run(use_reloader=True,debug=True)
